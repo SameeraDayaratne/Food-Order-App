fcbc452389ea52d966889b409909449637be70d3
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-01 23:53:33 +0530

    Implemented Header and Cart components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Cart(props) {
+  return (
+    <div>
+      <button className="text-yellow-500 text-xl font-bold">Cart (2)</button>
+    </div>
+  );
+}
+
+export default Cart;
