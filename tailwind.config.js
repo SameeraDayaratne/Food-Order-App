7b2e37863a0b34213c0b50c83deb57f438182336
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-12-02 14:51:19 +0530

    Implemented Static Meal Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        mealCard : '#1d1a16',
+        mealPrice : '#312c1d'
+      }
+    },
   },
   plugins: [],
 }
